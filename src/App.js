a9fc129acfb741e878b54b4cbe4294bc89ca7859
VaiDik-cyber <patelvaidik56@gmail.com>
2023-08-25 13:35:34 +0530

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [data, setData] = useState("Ram");
 
+  useEffect(() => {
+    console.log("useEffect run");
+  }, [data]);
   function increaseCount() {
     setCount(count + 1);
   }
@@ -12,13 +16,16 @@ function App() {
       setCount(count - 1);
     }
   }
-
+  function updateData() {
+    setData("Seeta");
+  }
   return (
     <>
       <h3>Your count is {count}</h3>
       <button onClick={increaseCount}>+</button>
 
       <button onClick={decreaseCount}>-</button>
+      <button onClick={updateData}>Update Data</button>
     </>
   );
 }
